23cb63874d00f52b383fa799b999c876090ddcb1
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-05-26 12:34:14 +0100

    Fix using API version
    Convention plugin sets a project property to be used by projects.
    Workaround for builds on TeamCity where the init script overwrites
    the version property in the TeamCity extension.

--- build-logic/src/main/kotlin/teamcity.base.gradle.kts
+++ build-logic/src/main/kotlin/teamcity.base.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id ("io.github.rodm.teamcity-base")
 }
 
+val teamcityVersion by extra((project.findProperty("teamcityVersion") ?: "2022.10") as String)
+
 teamcity {
-    version = (project.findProperty("teamcityVersion") ?: "2022.10") as String
+    version = teamcityVersion
 }
